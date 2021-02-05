3726debf8ad7c921b2fa4eb290d7456df5d2d9c1
임찬형 <ich0906@naver.com>
2021-02-05 15:09:25 +0900

    change login radio button into switchButton

--- app/src/main/java/com/harry/pullgo/LoginFragment.java
+++ app/src/main/java/com/harry/pullgo/LoginFragment.java
@@ -12,26 +12,37 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import lib.kingja.switchbutton.SwitchMultiButton;
+
 public class LoginFragment extends Fragment {
-    RadioButton studentButton;
-    RadioButton teacherButton;
+    SwitchMultiButton studentOrTeacher;
+    boolean isStudent=true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.fragment_login, container, false);
 
-        studentButton=rootView.findViewById(R.id.studentButton);
-        teacherButton=rootView.findViewById(R.id.teacherButton);
+        studentOrTeacher=rootView.findViewById(R.id.switchButton);
+        studentOrTeacher.setOnSwitchListener(new SwitchMultiButton.OnSwitchListener() {
+            @Override
+            public void onSwitch(int position, String tabText) {
+                if(position==0){
+                    isStudent=true;
+                }else if(position==1){
+                    isStudent=false;
+                }
+            }
+        });
 
         Button signUpButton=rootView.findViewById(R.id.buttonSignUp);
         signUpButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent=new Intent();
-                if(studentButton.isChecked()) {
+                if(isStudent) {
                     intent = new Intent(getContext(), StudentSignUpActivity.class);
-                }else if(teacherButton.isChecked()) {
+                }else if(!isStudent) {
                     intent = new Intent(getContext(), TeacherSignUpActivity.class);
                 }
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
