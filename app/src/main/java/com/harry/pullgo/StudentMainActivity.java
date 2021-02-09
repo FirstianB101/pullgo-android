46b5fc70e60904a6ee913ace305d9684de5a28f7
임찬형 <ich0906@naver.com>
2021-02-09 11:33:46 +0900

    add student navigation view

--- app/src/main/java/com/harry/pullgo/StudentMainActivity.java
+++ app/src/main/java/com/harry/pullgo/StudentMainActivity.java
@@ -2,10 +2,18 @@ package com.harry.pullgo;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.harry.pullgo.ui.ChangePersonInfo.ChangePersonInfoFragment;
+import com.harry.pullgo.ui.home.HomeFragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,32 +22,48 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class StudentMainActivity extends AppCompatActivity {
+public class StudentMainActivity extends AppCompatActivity
+                    implements NavigationView.OnNavigationItemSelectedListener,FragmentCallback{
 
-    private AppBarConfiguration mAppBarConfiguration;
+    DrawerLayout drawerLayout;
+    NavigationView studentNavigation;
+
+    HomeFragment studentHomeFragment;
+    ChangePersonInfoFragment studentChangeInfoFragment;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
-        Toolbar toolbar = findViewById(R.id.studentToolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar=getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.baseline_keyboard_backspace_white_18dp);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.NavigationViewStudent);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_student_home, R.id.nav_student_change_info)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.studentMainFragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        toolbar = findViewById(R.id.studentToolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        studentNavigation=findViewById(R.id.navigationViewStudent);
+        studentNavigation.setNavigationItemSelectedListener(this);
+
+        studentHomeFragment=new HomeFragment();
+        studentChangeInfoFragment=new ChangePersonInfoFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.studentMainFragment,studentHomeFragment).commit();
+        getSupportActionBar().setTitle("Home");
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -50,9 +74,31 @@ public class StudentMainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.studentMainFragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.nav_student_home:
+                onFragmentSelected(0,null);
+                break;
+            case R.id.nav_student_change_info:
+                onFragmentSelected(1,null);
+                break;
+        }
+        drawerLayout.closeDrawer(studentNavigation);
+
+        return true;
+    }
+
+    @Override
+    public void onFragmentSelected(int position, Bundle bundle) {
+        Fragment curFragment=null;
+
+        if(position==0){
+            curFragment=studentHomeFragment;
+            toolbar.setTitle("Home");
+        }else if(position==1){
+            curFragment=studentChangeInfoFragment;
+            toolbar.setTitle("회원정보 변경");
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.studentMainFragment,curFragment).commit();
     }
 }
